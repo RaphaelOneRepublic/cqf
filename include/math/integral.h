9e0ba2b4fe965ef4fb3bed6c365873d67c4da77f
Mingjie MA <mamingjieonerepublic@gmail.com>
2020-12-18 01:14:59 +0800

    coupon bonds yield to maturity & duration & convexity (newton's method) provide black_scholes implied volatility interface
    Signed-off-by: Mingjie MA <mamingjieonerepublic@gmail.com>

--- include/math/integral.h
+++ include/math/integral.h
@@ -85,17 +85,12 @@ template<typename Float, typename =floating_guard<Float>>
 inline static constexpr
 Float
 integrate_recur(const univariate_real_func<Float> &func, Float a, Float b, Float cur, Float prev, size_t n) {
-<<<<<<< HEAD
   return (
              /*acceptable error rate*/ abs(cur - prev) < limits<Float>::epsilon() * abs(cur) &&
           /*at least 16 partitions
            *2 iterations to get rid of the initial two guesses*/  n > CQF_MINIMUM_SIMPSON_PARTITION
          ) /*maximum partitions*/ || (n > CQF_MAXIMUM_SIMPSON_PARTITION) ? cur :
          /*double partition numbers*/ integrate_recur(func, a, b, integrate_part(func, a, b, n * 2), cur, n * 2);
-=======
-  return (abs(cur - prev) < limits<Float>::epsilon() * abs(cur) && n > 16) || (n > 65536) ? cur :
-         integrate_recur(func, a, b, integrate_part(func, a, b, n * 2), cur, n * 2);
->>>>>>> origin/master
 }
 
 } // namespace impl
@@ -112,11 +107,7 @@ integrate_recur(const univariate_real_func<Float> &func, Float a, Float b, Float
 template<typename Float, typename =floating_guard<Float>>
 inline static constexpr
 Float integrate(const univariate_real_func<Float> &func, Float a, Float b) {
-<<<<<<< HEAD
   return impl::integrate_recur(func, a, b, 1., 0., /*initial 4 partitions*/4);
-=======
-  return impl::integrate_recur(func, a, b, 1., 0., 4);
->>>>>>> origin/master
 }
 } // namespace cqf
 
